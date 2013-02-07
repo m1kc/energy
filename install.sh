3c394007815d8600bb01aef72e256ed60d87f225
m1kc <m1kc@yandex.ru>
2013-02-07 15:42:05 +0400

    New settings for installer. New preprocessor rule. A little cleanup.

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@
 # "111>>111"
 # 	will be replaced to
 # >>
+#
+# "111>111"
+# 	will be replaced to
+# >
 
 TMP=/tmp/.instvar
 JOURNAL=/tmp/.instlog
@@ -15,11 +19,7 @@ shell(){
 	echo == zsh
 }
 
-# Welcome!
-#echo Welcome to the Energy Linux installer.
-#echo Energy Linux is a lightweight Linux distribution based on Arch with batteries and other stuff included. Our goal is to create a distribution simple as Arch but works out of the box. We hope you will like it.
-#echo Press Enter to begin installation.
-#read
+### Welcome!
 dialog --msgbox "Welcome to the Energy Linux installer.
 
 Energy Linux is a lightweight Linux distribution based on Arch with batteries and other stuff included. Our goal is to create a distribution simple as Arch but works out of the box. We hope you will like it.
@@ -27,11 +27,6 @@ Energy Linux is a lightweight Linux distribution based on Arch with batteries an
 Click OK to begin installation." 0 0
 
 # Partition
-#echo At first we need to partition the hard drive. If you have no special needs, create one partition filling the whole disk.
-#echo Enter your hard drive name or leave blank "for" /dev/sda:
-#read disk
-#if [ -z $disk ]; then disk="/dev/sda"; fi
-#echo == cfdisk $disk
 disks1="`lsblk -r | grep disk | cut -d" " -f1`"
 disks=""
 for i in $disks1; do disks="${disks} /dev/${i} -"; done
@@ -48,20 +43,7 @@ case $disk in
 		;;
 esac
 
-# FS
-#echo Now we must create filesystems. Enter \"manual\" to manually create them, or enter desired partition to create ext4 on, or leave blank to create ext4 on ${disk}1:
-#read fs
-#case $fs in
-#	manual)
-#		shell
-#		;;
-#	"")
-#		echo == mkfs.ext4 ${disk}1
-#		;;
-#	*)
-#		echo == mkfs.ext4 $fs
-#		;;
-#esac
+### Filesystem
 partitions1="`lsblk -r | grep part | cut -d" " -f1`"
 partitions=""
 for i in $partitions1; do partitions="${partitions} /dev/${i} -"; done
@@ -78,20 +60,7 @@ case $partition in
 		;;
 esac
 
-# Mount
-#echo Now we need to mount new filesystems to /mnt. Enter \"manual\" to manually mount them, or enter desired partition to mount, or leave blank to mount ${disk}1:
-#read mou
-#case $mou in
-#	manual)
-#		shell
-#		;;
-#	"")
-#		echo == mount ${disk}1 /mnt
-#		;;
-#	*)
-#		echo == mount $mou /mnt
-#		;;
-#esac
+### Mount
 dialog --no-cancel --menu "Now we need to mount new filesystems to /mnt.
 
 Select a partition to mount or \"manual\" if you want command shell." 0 0 0 $partitions manual "" 2> $TMP
@@ -105,7 +74,7 @@ case $mou in
 		;;
 esac
 
-# Internet
+### Internet
 dialog --no-cancel --menu "It would be great to connect to the Internet to fetch latest updates. Of course, you can skip this step and install them later. Moreover, if you connect using wired connection like eth0, connection will be established automatically. In any of these cases, just type \"exit\" to leave command shell; otherwise configure your connection manually." 0 0 0 skip "Skip this step" wired "Just check it" wifi "Select wireless network" manual "" 2> $TMP
 internet=`cat $TMP`
 case $internet in
@@ -128,7 +97,7 @@ esac
 #echo You may want to edit /etc/pacman.d/mirrorlist
 # OR NOT
 
-# Pacstrap it
+### Pacstrap it
 dialog --msgbox "Okay, now we will pacstrap base packages to your new system. Just relax and wait." 0 0
 echo == pacstrap /mnt base
 
@@ -147,32 +116,40 @@ echo == arch-chroot /mnt pacman -S grub-bios
 #echo Now you must install a bootloader. Sorry, this part is not automated yet.
 #shell
 
-# fstab
+### fstab
 dialog --msgbox "Okay. Now we will generate fstab for your new system. Just relax and wait." 0 0
-echo == genfstab -p /mnt "111>>111" /mnt/etc/fstab
+echo == genfstab -U -p /mnt "111>>111" /mnt/etc/fstab
 
-# TODO: hostname
+### hostname
+dialog --inputbox "Please specify your hostname. It is okay to leave default one." 0 0 "myhost" 2> $TMP
+echo == cat $TMP "111>111" /mnt/etc/hostname
 
-# TODO: localtime
+# TODO: select localtime
+dialog --msgbox "Auto-selecting timezone Europe/Moscow. Hope it is OK." 0 0
+echo == ln -s /mnt/usr/share/zoneinfo/Europe/Moscow /mnt/etc/localtime
+# TODO: FUCK IT
 
-# TODO: locale
+# TODO: locale.conf
+# TODO: /etc/locale.gen and generate with locale-gen.
 
 # TODO: console font
 
-# TODO: /etc/locale.gen and generate with locale-gen.
-
-# TODO:      Configure /etc/mkinitcpio.conf as needed (see mkinitcpio) and create an initial RAM disk with: 
-# mkinitcpio -p linux
+### mkinitcpio
+# TODO: Configure /etc/mkinitcpio.conf as needed
+dialog --msgbox "Now we will create an initial RAM disk. Just relax and wait." 0 0
+echo == arch-chroot /mnt mkinitcpio -p linux
 
 # TODO: Configure the bootloader
 
-# TODO: set root password
+### Root password
+dialog --msgbox "Now we need to set root password." 0 0
+echo == arch-chroot /mnt passwd
 
 # TODO: edit pacman.conf
 
 # TODO: add user
 
-# Complete
+### Done!
 dialog --yesno "Installation complete! Reboot now?" 0 0
 if [ $? == 0 ]; then
 	echo == reboot
